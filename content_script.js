c0d8304ee2653a23c1d31b78f42f58f02e2a4a0c
Brian Attwell <brian@brianattwell.com>
2016-07-16 23:34:32 -0700

    Simplify. Support highlight secondary people
    No longer need to analyze the phab cookies. Now just ask people what
    usernames they want to highlight.

--- content_script.js
+++ content_script.js
@@ -1,7 +1,16 @@
-chrome.runtime.sendMessage({url: "https://" + window.location.hostname}, function(username) {
-  
-	$(".phui-object-item-col1 .phui-handle.phui-link-person:contains('attwell')")
-		.css( "text-decoration", "underline" )
-		.css( "font-size", "175%" );
+$(".phui-object-item-col1 .phui-handle.phui-link-person:contains('attwell')")
+	.css( "text-decoration", "underline" )
+	.css( "font-size", "175%" );
 
-});
+$(".phui-object-item-col1 .phui-handle.phui-link-person:contains('behrooz')")
+	.css( "text-decoration", "underline" )
+	.css( "font-size", "150%" );
+
+$(".phui-object-item-col1 .phui-handle.phui-link-person:contains('tonyc')")
+	.css( "text-decoration", "underline" )
+	.css( "font-size", "150%" );
+
+
+$(".phui-object-item-col1 .phui-handle.phui-link-person:contains('eleung')")
+	.css( "text-decoration", "underline" )
+	.css( "font-size", "150%" );
