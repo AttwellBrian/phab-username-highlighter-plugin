80b2251ed23e5f5f31b9c10da544b66aa5a79bb8
Brian Attwell <brian@brianattwell.com>
2016-07-16 23:35:19 -0700

    Simplify. Support highlight secondary people
    No longer need to analyze the phab cookies. Now just ask people what
    usernames they want to highlight.

--- content_script.js
+++ content_script.js
@@ -1,16 +1,13 @@
-$(".phui-object-item-col1 .phui-handle.phui-link-person:contains('attwell')")
-	.css( "text-decoration", "underline" )
-	.css( "font-size", "175%" );
+// Primary highlighting
+chrome.storage.sync.get(null, function (dict) {
+	$(".phui-object-item-col1 .phui-handle.phui-link-person:contains('" + dict.username + "')")
+		.css( "background-color", "#7FFF7F" )
+		.css( "text-decoration", "underline" );
 
-$(".phui-object-item-col1 .phui-handle.phui-link-person:contains('behrooz')")
-	.css( "text-decoration", "underline" )
-	.css( "font-size", "150%" );
+	dict.othernames.split("\n").forEach(function(value) {
+		$(".phui-object-item-col1 .phui-handle.phui-link-person:contains('" + value + "')")
+		.css( "background-color", "#ffa5a5" )
+		.css( "text-decoration", "underline" );
+	});
 
-$(".phui-object-item-col1 .phui-handle.phui-link-person:contains('tonyc')")
-	.css( "text-decoration", "underline" )
-	.css( "font-size", "150%" );
-
-
-$(".phui-object-item-col1 .phui-handle.phui-link-person:contains('eleung')")
-	.css( "text-decoration", "underline" )
-	.css( "font-size", "150%" );
+});
